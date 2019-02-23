5b3b0da2b58463f73f2a05a3945d3ee78772d3cb
Rain Wang <rainwzp@gmail.com>
2019-02-23 12:20:30 +0800

    mvc

---
+++ macula-boot/macula-boot-web/src/main/java/org/macula/boot/web/config/support/MaculaWebMvcConfigurer.java
@@ -0,0 +1,17 @@
+package org.macula.boot.web.config.support;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Rain
+ * @since 2019-2-23
+ */
+public class MaculaWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+
+}
