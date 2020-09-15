a4dc74f09e271a47b7109e60fdb33efa7922b2aa
Rain Wang <rainwzp@gmail.com>
2020-09-15 17:26:03 +0800

    调整klock配置

--- macula-samples/macula-samples-dubbo/src/main/java/org/macula/samples/dubbo/MaculaSamplesDubboApplication.java
+++ macula-samples/macula-samples-dubbo/src/main/java/org/macula/samples/dubbo/MaculaSamplesDubboApplication.java
@@ -34,6 +34,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo
 public class MaculaSamplesDubboApplication {
     public static void main(String[] args) {
         SpringApplication.run(MaculaSamplesDubboApplication.class);
